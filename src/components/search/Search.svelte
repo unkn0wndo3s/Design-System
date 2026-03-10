5feefdaa0edb0067e607908db8b74ab7740a3e84
LOUIS POTEVIN <louis.potevin@legrand.com>
2026-03-10 13:27:07 +0100

    Add Search component with TypeScript props and styles

---
+++ src/components/search/Search.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  interface Props {
+    disabled?: boolean;
+    placeholder: string;
+
+    children?: Snippet; 
+    /** The onclick event handler */
+    onclick?: () => void;
+  }
+
+  const { disabled, placeholder, onclick, ...props }: Props = $props();
+</script>
+
+<input
+  type="text"
+  class={[`search`, disabled && 'search--disabled'].join(' ')}
+  placeholder={placeholder}
+  {...props}
+  {disabled}
+  {onclick}
+/>
+
+<style lang="scss" global>
+  @import "./search.scss";
+</style>
