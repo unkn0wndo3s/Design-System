d0d4b0fe9249ceb0ea155b6e0521e29c76ca548c
LOUIS POTEVIN <louis.potevin@legrand.com>
2026-03-10 15:26:49 +0100

    Remove unused onclick prop from notification component

--- src/components/notification/Notification.svelte
+++ src/components/notification/Notification.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
   import type { Snippet } from 'svelte';
+  import Icon from '../icons/icon.svelte';
+
 
   interface Props {
     disabled?: boolean;
     type: "error" | "success" | "warning" | "info";
 
     children?: Snippet; 
-    /** The onclick event handler */
-    onclick?: () => void;
   }
 
-  const { type, onclick, children, ...props }: Props = $props();
+  const { type, children, ...props }: Props = $props();
 </script>
 
 <div class={[`notification`, `notification--${type}`]}
@@ -18,6 +18,7 @@
   {#if children}
     {@render children()}
   {/if}
+  <Icon type="close" size={24} />
 </div>
 
 <style lang="scss" global>
