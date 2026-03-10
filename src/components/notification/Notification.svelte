d54cd9814115111b23ea3aced4aabd70a3e0eaa2
LOUIS POTEVIN <louis.potevin@legrand.com>
2026-03-10 13:54:09 +0100

    Add Notification component with TypeScript props and styles

---
+++ src/components/notification/Notification.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  interface Props {
+    disabled?: boolean;
+    type: "error" | "success" | "warning" | "info";
+
+    children?: Snippet; 
+    /** The onclick event handler */
+    onclick?: () => void;
+  }
+
+  const { type, onclick, children, ...props }: Props = $props();
+</script>
+
+<div class={[`notification`, `notification--${type}`]}
+  {...props}>
+  {#if children}
+    {@render children()}
+  {/if}
+</div>
+
+<style lang="scss" global>
+  @import "./search.scss";
+</style>
