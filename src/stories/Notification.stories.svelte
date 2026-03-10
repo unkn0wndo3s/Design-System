098ac06e27e3f1f3bd7771af89b73d55844cd4e4
LOUIS POTEVIN <louis.potevin@legrand.com>
2026-03-10 13:53:41 +0100

    Add Notification component stories to Storybook

---
+++ src/stories/Notification.stories.svelte
@@ -0,0 +1,29 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Notification from '../components/notification/Notification.svelte';
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    title: 'Components/Notifications',
+    component: Notification,
+    tags: ['autodocs'],
+    argTypes: {
+      type: {
+        control: { type: 'select' },
+        options: ['error', 'success', 'info', 'warning'],
+      },
+    }
+  });
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Default" args={{ type: 'info' }} ></Story>
+
+
+<Story name="Information" args={{ type: 'info'}} ></Story>
+
+<Story name="Success" args={{ type: 'success' }} ></Story>
+
+<Story name="Warning" args={{ type: 'warning' }} ></Story>
+
+<Story name="Error" args={{ type: 'error' }} ></Story>
