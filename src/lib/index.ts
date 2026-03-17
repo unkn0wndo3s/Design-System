1b175daabf1200057170d7a3cf789579fe216cbb
unkn0wndo3s <unkn0wndo3s@gmail.com>
2026-03-17 21:48:45 +0100

    reexport entry components in index.ts

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,5 @@
 // Reexport your entry components here
+export { default as Button } from '../components/button/Button.svelte';
+export { default as Notification } from '../components/notification/Notification.svelte';
+export { default as Search } from '../components/search/Search.svelte';
+export { default as Tooltip } from '../components/tooltip/Tooltip.svelte';
