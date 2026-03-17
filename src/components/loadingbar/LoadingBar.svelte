acccb93ca45f915699f00635365c48da48268230
unkn0wndo3s <unkn0wndo3s@gmail.com>
2026-03-17 21:58:45 +0100

    bump version to 1.4.0-alpha; add LoadingBar component with customizable states and percentages

---
+++ src/components/loadingbar/LoadingBar.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  interface Props {
+    state?: 'unknown' | 'known';
+    percentage?: number;
+  }
+
+  const {
+    state = 'unknown',
+    percentage,
+    ...props
+  }: Props = $props();
+</script>
+
+<div class={[`loadingbar`, !percentage && `loadingbar--${state}`].filter(Boolean).join(' ')} {...props}>
+  <div class="loadingbar__background"></div>
+  <div
+    class="loadingbar__progress"
+    style={percentage !== undefined ? `width: ${percentage}%` : undefined}
+  ></div>
+</div>
+
+<style lang="scss" global>
+  @import "./loadingbar.scss";
+</style>
---
+++ src/stories/LoadingBar.stories.svelte
@@ -0,0 +1,35 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import LoadingBar from '../components/loadingbar/LoadingBar.svelte';
+
+  // More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+  const { Story } = defineMeta({
+    title: 'Components/LoadingBar',
+    component: LoadingBar,
+    tags: ['autodocs'],
+    argTypes: {
+      state: {
+        control: { type: 'select' },
+        options: ['unknown', 'known'],
+      },
+      percentage: {
+        control: { type: 'number', min: 0, max: 100 },
+      },
+    }
+  });
+</script>
+
+<!-- More on writing stories with args: https://storybook.js.org/docs/writing-stories/args -->
+<Story name="Default (Unknown)" args={{ state: 'unknown' }} />
+
+<Story name="Known" args={{ state: 'known' }} />
+
+<Story name="Percent0" args={{ percentage: 0 }} />
+
+<Story name="Percent25" args={{ percentage: 25 }} />
+
+<Story name="Percent50" args={{ percentage: 50 }} />
+
+<Story name="Percent75" args={{ percentage: 75 }} />
+
+<Story name="Percent100" args={{ percentage: 100 }} />
