ee425eb7536f1b414c989b22dee4f1dfe0d2d9b2
unkn0wndo3s <unkn0wndo3s@gmail.com>
2026-03-17 21:47:10 +0100

    add Tooltip component with customizable positions and styles

---
+++ src/components/tooltip/Tooltip.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import type { Snippet } from 'svelte';
+
+  interface Props {
+    position?:
+      | 'bottom-right'
+      | 'bottom-left'
+      | 'top-right'
+      | 'top-left'
+      | 'right'
+      | 'left'
+      | 'bottom'
+      | 'top';
+    children?: Snippet; 
+  }
+
+  const {
+    position = 'bottom-right',
+    children,
+    ...props
+  }: Props = $props();
+</script>
+
+<div class={[`tooltip`, `tooltip--${position}`].join(' ')} role="tooltip" {...props}>
+    <span class="tooltip__text">
+  {#if children}
+    {@render children()}
+    
+  {/if}
+  </span>
+</div>
+
+<style lang="scss" global>
+  @import './tooltip.scss';
+</style>
---
+++ src/stories/Tooltip.stories.svelte
@@ -0,0 +1,26 @@
+<script module>
+  import { defineMeta } from '@storybook/addon-svelte-csf';
+  import Tooltip from '../components/tooltip/Tooltip.svelte';
+
+  const { Story } = defineMeta({
+    title: 'Components/Tooltip',
+    component: Tooltip,
+    tags: ['autodocs'],
+    argTypes: {
+      position: {
+        control: { type: 'select' },
+        options: ['bottom-right', 'bottom-left', 'top-right', 'top-left', 'right', 'left', 'bottom', 'top'],
+      },
+      text: { control: { type: 'text' } },
+    },
+  });
+</script>
+
+<Story name="Bottom Right" args={{ position: 'bottom-right' }}>lorem ipsum dolore  ...</Story>
+<Story name="Bottom Left" args={{ position: 'bottom-left' }}>lorem ipsum dolore  ...</Story>
+<Story name="Top Right" args={{ position: 'top-right' }}>lorem ipsum dolore  ...</Story>
+<Story name="Top Left" args={{ position: 'top-left' }}>lorem ipsum dolore  ...</Story>
+<Story name="Right" args={{ position: 'right' }}>lorem ipsum dolore  ...</Story>
+<Story name="Left" args={{ position: 'left' }}>lorem ipsum dolore  ...</Story>
+<Story name="Bottom" args={{ position: 'bottom' }}>lorem ipsum dolore  ...</Story>
+<Story name="Top" args={{ position: 'top' }}>lorem ipsum dolore  ...</Story>
