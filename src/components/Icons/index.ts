18702da0b640fe93abace97851250c1980305531
P2Wdisabled <louis1.potevin1@gmail.com>
2026-05-15 18:51:05 +0200

    feat: update icon exports and add Search icon with multiple sizes

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,3 +1,4 @@
-export { default as Arrow2 } from './Arrow2/Arrow2.astro';
-export { default as Profile } from './profile/profile.astro';
-export { default as Share } from './share/share.astro';
+export { default as Arrow2Icon } from './Arrow2/Arrow2.astro';
+export { default as ProfileIcon } from './profile/profile.astro';
+export { default as ShareIcon } from './share/share.astro';
+export { default as SearchIcon } from './search/search.astro';
